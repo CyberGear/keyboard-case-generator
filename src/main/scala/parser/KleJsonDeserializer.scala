cddb9d1d27e0bbcc5140ab738ec0c1653f420be3
Marius Kavoliunas <kavoliunas.m@gmail.com>
2023-10-01 20:36:17 +0300

    fix latest issues on layout deserializer

--- src/main/scala/parser/KleJsonDeserializer.scala
+++ src/main/scala/parser/KleJsonDeserializer.scala
@@ -19,34 +19,50 @@ class KleJsonDeserializer extends JsonDeserializer[Layout] {
 
   @tailrec
   private def readLayout(input: String, rows: List[model.Row] = Nil): Layout = input match {
-    case Row(row, group) => readLayout(input.drop(row.length + 1).trim, rows :+ readKeys(s"${group.trim},"))
+    case Row(row, group) =>
+      readLayout(
+        input.drop(row.length + 1).trim,
+        rows :+ readKeys(s"${group.trim},", rows.lastOption.map(_.keys.head))
+      )
     case _               => Layout(rows)
   }
 
   @tailrec
-  private def readKeys(row: String, keys: List[Key] = Nil): model.Row = {
-    println(row + " | " + keys)
+  private def readKeys(row: String, previousRowKey: Option[Key], keys: List[Key] = Nil): model.Row =
     row match {
       case Mods(block, mods) =>
-        println(s">> $block | $mods")
-        readKeys(row.drop(block.length).trim, keys :+ nextKey(keys.lastOption, parseMods(mods)))
+        readKeys(
+          row.drop(block.length).trim,
+          previousRowKey,
+          keys :+ nextKey(previousRowKey, keys.lastOption, parseMods(mods))
+        )
       case Switch(letter)    =>
-        println(s">> $letter")
-        readKeys(row.drop(letter.length).trim, keys :+ nextKey(keys.lastOption))
+        readKeys(
+          row.drop(letter.length).trim,
+          previousRowKey,
+          keys :+ nextKey(previousRowKey, keys.lastOption)
+        )
       case _                 => model.Row(keys)
     }
-  }
 
   private def parseMods(modsString: String): Map[String, String] =
-    modsString.split(",").map(_.split(":")).map(p => p.head.strip() -> p.last.strip()).toMap
+    modsString
+      .split(",")
+      .map(_.split(":"))
+      .map(p => p.head.strip() -> p.last.strip())
+      .toMap
 
-  private def nextKey(previous: Option[Key], args: Map[String, String] = Map.empty): Key = {
-    val position = previous.map(_.position).getOrElse(Position(0, 0))
-    val size     = previous.map(_.size).getOrElse(Size(0, 0))
+  private def nextKey(previousRowKey: Option[Key], previous: Option[Key], args: Map[String, String] = Map.empty): Key = {
+    val xPos = previous.map(_.position).getOrElse(Position(0, 0))
+    val xSize = previous.map(_.size).getOrElse(Size(0, 0))
+
+    val yPos = previousRowKey.map(_.position).getOrElse(Position(0, 0))
+    val ySize = previousRowKey.map(_.size).getOrElse(Size(0, 0))
+
     Key(
       Position(
-        position.x + size.width + BigDecimal(args.getOrElse("x", "0")),
-        position.y + BigDecimal(args.getOrElse("y", "0"))
+        xPos.x + xSize.width + BigDecimal(args.getOrElse("x", "0")),
+        previous.map(_.position.y).getOrElse(yPos.y + ySize.height + BigDecimal(args.getOrElse("y", "0")))
       ),
       Size(
         BigDecimal(args.getOrElse("w", "1")),
--- src/test/scala/KleLayoutParserTest.scala
+++ src/test/scala/KleLayoutParserTest.scala
@@ -1,4 +1,5 @@
 import model.{Key, Layout, Row}
+import org.scalatest.AppendedClues.convertToClueful
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import parser.YamlMapper
@@ -8,67 +9,141 @@ import scala.language.implicitConversions
 class KleLayoutParserTest extends AnyFlatSpec with Matchers {
 
   it should "parse yaml" in {
-    val actual = YamlMapper.readValue[Layout](asYamlString(
-      """      [ { x:2.25,c:"#ffffff",p:"XDA",a:7},"Esc",{x:0.25},"F1","F2","F3","F4",{x:0.25},"F5","F6"],
+    val actual = YamlMapper.readValue[Layout](
+      asYamlString(
+        """      [ { x:2.25,c:"#ffffff",p:"XDA",a:7},"Esc",{x:0.25},"F1","F2","F3","F4",{x:0.25},"F5","F6"],
         |      [ { y:0.25 },"M1","M2",{ x:0.25,a:5 },"~\n`","!\n1","@\n2","#\n3","$\n4","%\n5","^\n6" ],
         |      [ { a:7 },"M3","M4",{ x:0.25,w:1.5 },"Tab","Q","W","E","R","T" ],
         |      [ "M5","M6",{ x:0.25,w:1.25,w2:1.75,l:true },"Caps Lock",{ x:-1.25,w:1.75 },"Caps Lock","A","S","D","F","G" ],
         |      [ "M7","M8",{ x:0.25,w:2.25 },"Shift","Z","X","C","V","B" ],
         |      [ "M9","M0",{ x:0.25,w:1.25 },"Ctrl","Fn",{ w:1.25 },"Win",{ w:1.25 },"Alt",{ w:2.5 },"" ]
         |""".stripMargin
-    ))
+      )
+    )
 
     actual.rows should not be Nil
   }
 
   it should "correct number of rows" in {
-    val actual = YamlMapper.readValue[Layout](asYamlString(
-      """[ "" ], [ "" ], [ "" ], [ "" ], [ "" ], [ "" ]"""
-    ))
+    val actual = YamlMapper.readValue[Layout](
+      asYamlString(
+        """[ "" ], [ "" ], [ "" ], [ "" ], [ "" ], [ "" ]"""
+      )
+    )
 
     actual.rows.length should be(6)
   }
 
   it should "correct number of columns" in {
-    YamlMapper.readValue[Layout](asYamlString(
-      """[ "", "", "", "", "", "", "" ]"""
-    )).rows.head.keys.length should be(7)
+    YamlMapper
+      .readValue[Layout](
+        asYamlString(
+          """[ "", "", "", "", "", "", "" ]"""
+        )
+      )
+      .rows
+      .head
+      .keys
+      .length should be(7)
 
-    YamlMapper.readValue[Layout](asYamlString(
-      """[ "A", "B" ]"""
-    )).rows.head.keys.length should be(2)
+    YamlMapper
+      .readValue[Layout](
+        asYamlString(
+          """[ "A", "B" ]"""
+        )
+      )
+      .rows
+      .head
+      .keys
+      .length should be(2)
 
-    YamlMapper.readValue[Layout](asYamlString(
-      """[ "A", {w:10},"B" ]"""
-    )).rows.head.keys.length should be(2)
+    YamlMapper
+      .readValue[Layout](
+        asYamlString(
+          """[ "A", {w:10},"B" ]"""
+        )
+      )
+      .rows
+      .head
+      .keys
+      .length should be(2)
   }
 
   it should "read correct width and position" in {
-    YamlMapper.readValue[Layout](asYamlString(
-      """[ "", "", "", "", "", "", "" ]"""
-    )).rows.head.keys.last.position.x should be(6)
+    YamlMapper
+      .readValue[Layout](
+        asYamlString(
+          """[ "", "", "", "", "", "", "" ]"""
+        )
+      )
+      .rows
+      .head
+      .keys
+      .last
+      .position
+      .x should be(6)
 
-    YamlMapper.readValue[Layout](asYamlString(
-      """[ "", {x:3.75, w:0.25}, "", "" ]"""
-    )).rows.head.keys.last.position.x should be(5)
+    YamlMapper
+      .readValue[Layout](
+        asYamlString(
+          """[ "", {x:3.75, w:0.25}, "", "" ]"""
+        )
+      )
+      .rows
+      .head
+      .keys
+      .last
+      .position
+      .x should be(5)
 
-    YamlMapper.readValue[Layout](asYamlString(
-      """[ {x:3.75}, "", {w:0.25}, "", "" ]"""
-    )).rows.head.keys.last.position.x should be(5)
+    YamlMapper
+      .readValue[Layout](
+        asYamlString(
+          """[ {x:3.75}, "", {w:0.25}, "", "" ]"""
+        )
+      )
+      .rows
+      .head
+      .keys
+      .last
+      .position
+      .x should be(5)
   }
 
-  it should "read complete keyboard" in {
-    YamlMapper.readValue[Layout](asYamlString(
-      """[ "", {x:2}, "" ],
+  it should "read custom horizontal keyboard alignment" in {
+    val layout = YamlMapper.readValue[Layout](
+      asYamlString(
+        """[ "", {x:2}, "" ],
         |[ {x:1,w:2}, "" ],
         |[ "", {x:0.5}, "", {x:0.5}, "" ],
         |[ {x:0.75}, "", {x:0.5}, "" ]""".stripMargin
-    )) should be(
+      )
+    ) should be(
       Layout(
-        Row( Key(), Key(x = 3) ),
-        Row( Key(x = 1, w = 2) ),
-        Row( Key(), Key(x = 1.5), Key(x = 3) ),
-        Row( Key(x = 0.75), Key(x = 2.25) )
+        Row(Key(), Key(x = 3)),
+        Row(Key(x = 1, y = 1, w = 2)),
+        Row(Key(y = 2), Key(x = 1.5, y = 2), Key(x = 3, y = 2)),
+        Row(Key(x = 0.75, y = 3), Key(x = 2.25, y = 3))
+      )
+    )
+  }
+
+  it should "read custom vertical keyboard alignment" in {
+    YamlMapper.readValue[Layout](
+      asYamlString(
+        """[ "", {x:2}, "" ],
+          |[ {y:-0.5,x:1,w:2}, "" ],
+          |[ {y:-0.5}, "", {x:2}, "" ],
+          |[ {y:0.25,w:4}, "" ],
+          |[ {y:0.25,w:1}, "", "", "", "" ]""".stripMargin
+      )
+    ) should be(
+      Layout(
+        Row(Key(), Key(x = 3)),
+        Row(Key(x = 1, y = 0.5, w = 2)),
+        Row(Key(y = 1), Key(x = 3, y = 1)),
+        Row(Key(y = 2.25, w = 4)),
+        Row(Key(y = 3.5), Key(x = 1, y = 3.5), Key(x = 2, y = 3.5), Key(x = 3, y = 3.5))
       )
     )
   }
