f0740593b6b2809c9dc27de136bd0a643cd96bfd
Marius Kavoliunas <kavoliunas.m@gmail.com>
2023-10-26 22:46:47 +0300

    implement initial proC mount

---
+++ src/main/scala/custom/McuProC.scala
@@ -0,0 +1,46 @@
+package custom
+
+import scadla.InlineOps.{AngleConversions, Ops}
+import scadla.{Cube, Cylinder, Solid}
+import squants.space.LengthConversions.LengthConversions
+
+//noinspection NonAsciiCharacters
+object McuProC {
+
+  private val pcbX  = 19.mm
+  private val pcbY  = 35.5.mm
+  private val pcbZ  = 1.8.mm
+  private val usbCx = 9.mm
+  private val usbCz = 3.6.mm
+
+  def apply(): Solid =
+    Cube(pcbX, pcbY, pcbZ) +
+      AdvCube(usbCx, 7.35.mm, usbCz, xzr = Some(usbCz / 2)).move((pcbX - usbCx) / 2, 29.1.mm, pcbZ) +
+      Cube(12.5.mm, 27.3.mm, 3.3.mm).moveX((pcbX - 12.5.mm) / 2).moveY(1.8.mm)
+
+  def cutOut(): Solid = {
+    val initialShape = List[Solid](
+      Cube(pcbX, pcbY, pcbZ * 3),
+      Cube(pcbX, pcbY, pcbZ * 2).moveZ(pcbZ).moveY(-2.mm),
+      AdvCube(usbCx, 17.35.mm, usbCz, xzr = Some(usbCz / 2)).move((pcbX - usbCx) / 2, 29.1.mm, pcbZ),
+    ).reduce(_ + _)
+    val snap = Cylinder(1.5.mm, pcbX / 2).rotateY(90.°).move(pcbX / 4, -1.1.mm, pcbZ + 1.mm)
+    val negativeSpace = List[Solid](
+      Cube(0.5.mm, 2.mm, pcbZ).moveY(-2.mm).moveX(pcbX / 4 - 0.5.mm),
+      Cube(0.5.mm, 2.mm, pcbZ).moveY(-2.mm).moveX(pcbX / 4 * 3),
+      Cube(pcbX / 2 + 1.mm, 1.mm, pcbZ*3).moveX(pcbX / 4 - 0.5.mm).moveY(-3.mm),
+      AdvCube(15.mm, 10.mm, 10.mm, xzr = Some(2.mm)).moveX((pcbX - 15.mm) / 2).moveY(pcbY + 1.5.mm).moveZ(-2.mm)
+    ).reduce(_ + _)
+
+    (initialShape - snap + negativeSpace).moveX(-pcbX / 2).moveY(-pcbY - 1.5.mm)
+  }
+
+  def addOn(): Solid = {
+    val backBrace = Cube(pcbX, 3.mm, pcbZ * 3).moveY(-2.mm)
+    val frontPanel = Cube(pcbX + 4.mm, 1.5.mm, 8.5.mm).moveY(pcbY).moveX(-2.mm)
+    val frontPanelBrace1 = Cube(2.mm, 4.mm, pcbZ * 3).moveY(pcbY - 4.mm).moveX(-2.mm)
+    val frontPanelBrace2 = Cube(2.mm, 4.mm, pcbZ * 3).moveY(pcbY - 4.mm).moveX(pcbX)
+    (backBrace + frontPanel + frontPanelBrace1 + frontPanelBrace2).moveX(-pcbX / 2).moveY(-pcbY - 1.5.mm)
+  }
+
+}
--- src/test/scala/GeneratorTest.scala
+++ src/test/scala/GeneratorTest.scala
@@ -2,20 +2,21 @@ import model.inputmodel.Keyboard
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import parser.YamlMapper
+
 import scala.language.postfixOps
 
 class GeneratorTest extends AnyFlatSpec with Matchers with Utils {
 
   it should "generate something" taggedAs Wip in {
     val layout = readLayoutYaml(
-        """---
-          |layout: >
-          |  ["-","*","/","Num Lock"],
-          |  [{h:2},"+","7\nHome","8\n↑","9\nPgUp"],
-          |  [{x:1},"4\n←","5","6\n→"],
-          |  [{h:2},"Enter","1\nEnd","2\n↓","3\nPgDn"],
-          |  [{x:1},".\nDel",{w:2},"0\nIns"]
-          |""".stripMargin
+      """---
+        |layout: >
+        |  ["-","*","/","Num Lock"],
+        |  [{h:2},"+","7\nHome","8\n↑","9\nPgUp"],
+        |  [{x:1},"4\n←","5","6\n→"],
+        |  [{h:2},"Enter","1\nEnd","2\n↓","3\nPgDn"],
+        |  [{x:1},".\nDel",{w:2},"0\nIns"]
+        |""".stripMargin
     )
 
     val keyboard     = YamlMapper.readValue[Keyboard](ClassLoader.getSystemResource("MK-LTK.yml"))
@@ -26,10 +27,6 @@ class GeneratorTest extends AnyFlatSpec with Matchers with Utils {
 //    Util.storeCase("/home/marius/Documents", generator.generateCase.copy(name = s"kb-1"))
 
     Util.preview(generator.generateCase.blocks.head.parts.head.solid)
-  }
-
-  it should "draw mc-pro-c" in {
-    // TODO: impl
   }
 
 }
