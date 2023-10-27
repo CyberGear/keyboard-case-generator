aa7d1f82edeac5c1e9a387bc22e24f5e4caca399
Marius Kavoliunas <kavoliunas.m@gmail.com>
2023-10-27 08:59:20 +0300

    define pro-c pcu

--- src/main/scala/custom/McuProC.scala
+++ src/main/scala/custom/McuProC.scala
@@ -8,7 +8,7 @@ import squants.space.LengthConversions.LengthConversions
 object McuProC {
 
   private val pcbX  = 19.mm
-  private val pcbY  = 35.5.mm
+  private val pcbY  = 35.mm
   private val pcbZ  = 1.8.mm
   private val usbCx = 9.mm
   private val usbCz = 3.6.mm
@@ -29,6 +29,7 @@ object McuProC {
       Cube(0.5.mm, 2.mm, pcbZ).moveY(-2.mm).moveX(pcbX / 4 - 0.5.mm),
       Cube(0.5.mm, 2.mm, pcbZ).moveY(-2.mm).moveX(pcbX / 4 * 3),
       Cube(pcbX / 2 + 1.mm, 1.mm, pcbZ*3).moveX(pcbX / 4 - 0.5.mm).moveY(-3.mm),
+      Cube(pcbX / 2, 3.mm, 1.mm).move(pcbX / 4, -2.mm, 3.25.mm),
       AdvCube(15.mm, 10.mm, 10.mm, xzr = Some(2.mm)).moveX((pcbX - 15.mm) / 2).moveY(pcbY + 1.5.mm).moveZ(-2.mm)
     ).reduce(_ + _)
 
@@ -37,9 +38,9 @@ object McuProC {
 
   def addOn(): Solid = {
     val backBrace = Cube(pcbX, 3.mm, pcbZ * 3).moveY(-2.mm)
-    val frontPanel = Cube(pcbX + 4.mm, 1.5.mm, 8.5.mm).moveY(pcbY).moveX(-2.mm)
-    val frontPanelBrace1 = Cube(2.mm, 4.mm, pcbZ * 3).moveY(pcbY - 4.mm).moveX(-2.mm)
-    val frontPanelBrace2 = Cube(2.mm, 4.mm, pcbZ * 3).moveY(pcbY - 4.mm).moveX(pcbX)
+    val frontPanel = AdvCube(pcbX + 4.mm, 1.5.mm, 8.5.mm, xzr = Some(1.mm)).moveY(pcbY).moveX(-2.mm)
+    val frontPanelBrace1 = AdvCube(2.mm, 5.mm, pcbZ * 3, xyr = Some(1.mm), yzr = Some(1.mm)).moveY(pcbY - 4.mm).moveX(-2.mm)
+    val frontPanelBrace2 = AdvCube(2.mm, 5.mm, pcbZ * 3, xyr = Some(1.mm), yzr = Some(1.mm)).moveY(pcbY - 4.mm).moveX(pcbX)
     (backBrace + frontPanel + frontPanelBrace1 + frontPanelBrace2).moveX(-pcbX / 2).moveY(-pcbY - 1.5.mm)
   }
 
